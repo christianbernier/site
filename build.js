a191a2e269269d021810c26a996f3577c6236418
Christian Bernier <christianbernier02@gmail.com>
2022-02-15 22:54:49 -0500

    write files

--- build.js
+++ build.js
@@ -1,23 +1,23 @@
 // build.js
 // builds the public/ folder using contents of the src/ folder, populating the portfolio in index.html
 
-const fs = require("fs")
+const fs = require("fs-extra")
 
 // copy styles.css to /public
 const css = fs.readFileSync("src/styles.css")
-fs.writeFileSync(__dirname + "/public/styles.css", css)
+fs.outputFileSync(__dirname + "/public/styles.css", css)
 
 // copy favicon.png to /public
 const favicon = fs.readFileSync("src/favicon.png")
-fs.writeFileSync(__dirname + "/public/favicon.png", favicon)
+fs.outputFileSync(__dirname + "/public/favicon.png", favicon)
 
 // copy shareimage.png to /public
 const shareimage = fs.readFileSync("src/shareimage.png")
-fs.writeFileSync(__dirname + "/public/shareimage.png", shareimage)
+fs.outputFileSync(__dirname + "/public/shareimage.png", shareimage)
 
 // copy link.png to /public
 const linkimage = fs.readFileSync("src/link.png")
-fs.writeFileSync(__dirname + "/public/link.png", linkimage)
+fs.outputFileSync(__dirname + "/public/link.png", linkimage)
 
 // get index.html template and populate with projects from projects.json
 const template = fs.readFileSync("src/index.html", "utf8")
@@ -41,4 +41,4 @@ for(const project of projects.projects) {
 }
 
 html = template.replace("PROJECTS", projectsHtml)
-fs.writeFileSync(__dirname + "/public/index.html", html)
+fs.outputFileSync(__dirname + "/public/index.html", html)
